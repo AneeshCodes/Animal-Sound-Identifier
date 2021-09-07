baf3616c8615729b3c762b935662c1df28dec638
AneeshCodes <74843895+AneeshCodes@users.noreply.github.com>
2021-09-06 17:05:13 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+var barking = 0
+var meowing = 0
+var mooing = 0
+var roaring = 0
+var bg = 0 
+
 function start()
 {
   navigator.mediaDevices.getUserMedia({audio: true});
@@ -10,11 +16,48 @@ function modelReady()
   classifier.classify(gotResults)
 }
 
-function gotResults(error, results){
+function gotResults(error, results)
+{
   if(error){
     console.log(error)
   }
   else{
     console.log(results)
-  }
+    r = Math.floor(Math.random()*255) + 1;
+    g = Math.floor(Math.random()*255) + 1;
+    b = Math.floor(Math.random()*255) + 1;  
+
+    var result_label = results[0].label 
+    var image = document.getElementById('image')
+    if(result_label == "Background Noise"){
+      bg = bg + 1
+      image.src = "Ear.png";
+      bg_text = "Detected Background Noise = " + bg;
+      document.getElementById('number_of_times').innerHTML = + bg_text
+    }
+    if(result_label == "Barking"){
+      barking = barking + 1
+      image.src = "Dog.jpeg";
+      barking_text = "Detected Barking Noise = " + barking;
+      document.getElementById('number_of_times').innerHTML = + barking_text
+    }
+    if(result_label == "Meowing"){
+      meowing = meowing + 1
+      image.src = "Cat.jpeg";
+      meowing_text = "Detected Meowing Noise = " + meowing;
+      document.getElementById('number_of_times').innerHTML = + meowing_text
+    }
+    if(result_label == "Mooing"){
+      mooing = mooing + 1
+      image.src = "Cow.jpg";
+      mooing_text = "Detected Mooing Noise = " + mooing;
+      document.getElementById('number_of_times').innerHTML = + mooing_text
+    }
+    if(result_label == "Roaring"){
+      roaring = roaring + 1
+      image.src = "Lion.jpeg";
+      roaring_text = "Detected Roaring Noise = " + roaring;
+      document.getElementById('number_of_times').innerHTML = + roaring_text
+    }
+  }  
 }
